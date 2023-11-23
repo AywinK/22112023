8392fd273a5cb90003bb472856c3f9ebce1e4402
Aywin <AywinK@hotmail.com>
2023-11-23 20:36:07 +0000

    slight cleanup

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,10 +8,20 @@ const Form = () => {
 
     console.log(username, password);
 
+
+    const inputStylesObj = {
+        display: "block",
+        marginTop: "10px",
+        width: "350px",
+        height: "1.5rem",
+        padding: "15px",
+        fontSize: "1.2rem",
+        borderRadius: "24px"
+    };
     return (
         <>
-            <input type="text" value={username} onChange={(e) => { setUsername(() => e.target.value) }} style={{ display: "block", marginTop: "10px", width: "350px", height: "1.5rem", padding: "15px", fontSize: "1.2rem", borderRadius: "24px" }} />
-            <input type="password" value={password} onChange={(e) => { setPassword(() => e.target.value) }} style={{ display: "block", marginTop: "10px", width: "350px", height: "1.5rem", padding: "15px", fontSize: "1.2rem", borderRadius: "24px" }} />
+            <input type="text" value={username} onChange={(e) => { setUsername(() => e.target.value) }} style={inputStylesObj} />
+            <input type="password" value={password} onChange={(e) => { setPassword(() => e.target.value) }} style={inputStylesObj} />
             <button onClick={() => setIsDisplayed(prev => !prev)}>{isDisplayed ? "hide" : "show"}</button>
             {isDisplayed && <p>Username: {username}<br />Password: {password}</p>}
         </>
